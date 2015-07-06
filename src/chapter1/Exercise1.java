4f22182b3a68b005c95663e50a4b89990d9b07f7
chens <e_shchen@163.com>
2015-07-06 22:22:05 +0800

    6 Jul 15

--- src/chapter1/Exercise1.java
+++ src/chapter1/Exercise1.java
@@ -5,6 +5,8 @@ package chapter1;
  */
 public class Exercise1 {
     public static void main(String[] args) {
-        //
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
     }
 }
---
+++ src/chapter1/Exercise2.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+/**
+ * Created by chensmac on 15/7/6.
+ */
+public class Exercise2 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
