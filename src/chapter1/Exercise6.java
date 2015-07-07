7279cab954b6761e0edc4a8bda142ee070d05be3
chens <e_shchen@163.com>
2015-07-07 15:25:19 +0800

    add exercise 5 & 6.

---
+++ src/chapter1/Exercise6.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+/**
+ * Created by chens on 2015/7/7.
+ */
+public class Exercise6 {
+    public static void main(String[] args) {
+        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9));
+    }
+}
