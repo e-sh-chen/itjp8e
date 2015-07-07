2eb11fe500465be196cd05939f0decfa41316823
chens <e_shchen@163.com>
2015-07-07 15:34:52 +0800

    add exercise 7.

---
+++ src/chapter1/Exercise7.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+/**
+ * Created by chens on 2015/7/7.
+ */
+public class Exercise7 {
+    public static void main(String[] args) {
+        System.out.println("Pi is " + (4 * (1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13 - 1.0 / 15 + 1.0 / 17)));
+    }
+}
